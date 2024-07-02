f69dc25ddb0ef54c0e1a65161aec547f75a29547
summenurfatema <shadrinmoni15@gmail.com>
2024-07-02 16:21:44 +0600

    code 1

--- mybuddy-client/src/Pages/GeneralDashboard/GeneralRequest/GeneralRequest.jsx
+++ mybuddy-client/src/Pages/GeneralDashboard/GeneralRequest/GeneralRequest.jsx
@@ -13,7 +13,7 @@ const GeneralRequest = () => {
 
 //   const fetchClientSecret = async () => {
 //     try {
-//       const response = await fetch('http://ec2-3-80-196-219.compute-1.amazonaws.com/api/v1/payment/create-payment-intent', {
+//       const response = await fetch('http://ec2-18-213-245-65.compute-1.amazonaws.com/api/v1/payment/create-payment-intent', {
 //         method: 'POST',
 //         headers: {
 //           'Content-Type': 'application/json',
--- mybuddy-client/src/features/api/apiSlice.js
+++ mybuddy-client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://ec2-3-80-196-219.compute-1.amazonaws.com/api/v1"  
+    baseUrl: "http://ec2-18-213-245-65.compute-1.amazonaws.com/api/v1"  
   }),
   endpoints: (builder) => ({}),
   tagTypes: ["Memmer","SingleMember","Post","SingleMemberPost","Friend","Project","ProjectJoinRequest","Payment","Skill","SingleMemberSkill","SocialInfo","SingleMemberSocialInfo","License","SingleMemberLicense","Experience","SingleMemberExperience"],
